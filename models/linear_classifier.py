8fec94aed0a96bb8a5f19377e86125f6d876b450
xpomin <s-hromin@mail.ru>
2024-10-17 11:30:04 +0700

    add linear classifier and sgd classifier

---
+++ models/linear_classifier.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from .estimator import Estimator
+
+class LinearClassifier(Estimator):
+    """
+    Линейный классификатор.
+    """
+
+    def __init__(self, fit_intercept=True):
+        self.weights = None
+        self.bias = None
+        self.fit_intercept = fit_intercept
+
+    def fit(self, X, y):
+        """
+        Обучает линейный регрессор на данных X и целевой переменной y.
+
+        Параметры:
+        X : array-like, shape (n_samples, n_features)
+            Входные данные для обучения.
+        y : array-like, shape (n_samples,)
+            Целевая переменная.
+        """
+        n_samples, n_features = X.shape
+
+        self.weights = np.zeros(n_features)
+        if self.fit_intercept:
+            X_b = np.hstack([np.ones((X.shape[0], 1)), X])
+        else:
+            X_b = np.copy(X)
+            
+        self.weights = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
+    def predict(self, X):
+        """
+        Предсказывает значения для входных данных X.
+
+        Параметры:
+        X : array-like, shape (n_samples, n_features)
+            Входные данные для предсказания.
+
+        Возвращает:
+        array-like, shape (n_samples,)
+            Предсказанные значения.
+        """
+        X_df = pd.DataFrame(X)
+        if self.fit_intercept:
+            X_df.insert(0, 'Intercept', 1) 
+
+        return (X_df.dot(self.weights) >= 0).astype(int)
--- models/sgd_classifier.py
+++ models/sgd_classifier.py
@@ -1,18 +1,23 @@
-from .estimator import Estimator
 import numpy as np
-from utils import mean_squared_error
+from .estimator import Estimator
 
-class SGDRegressor(Estimator):
+class SGDClassifier(Estimator):
+    """
+    Линейный классификатор (логистическая регрессия).
+    """
+
     def __init__(self, learning_rate=0.01, n_iterations=1000, fit_intercept=True):
-        self.weights = None
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
-        self.bias = None
         self.fit_intercept = fit_intercept
+        self.weights = None
+
+    def _sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
     def fit(self, X, y):
         """
-        Обучает линейный регрессор на данных X и целевой переменной y.
+        Обучает линейный классификатор на данных X и целевой переменной y.
 
         Параметры:
         X : array-like, shape (n_samples, n_features)
@@ -22,21 +27,22 @@ class SGDRegressor(Estimator):
         """
         n_samples, n_features = X.shape
 
-        self.weights = np.zeros(n_features)
         if self.fit_intercept:
-            X_b = np.hstack([np.ones((X.shape[0], 1)), X])
+            X_b = np.hstack([np.ones((n_samples, 1)), X])
         else:
-            X_b = np.copy(X)
-            
+            X_b = X
+        
+        self.weights = np.zeros(X_b.shape[1])
+
         for _ in range(self.n_iterations):
-            y_pred = X_b.dot(self.weights)
-            
-            gradients = -(2/n_samples) * X_b.T.dot(y - y_pred)
+            y_pred = self._sigmoid(X_b.dot(self.weights))
+
+            gradients = (1/n_samples) * X_b.T.dot(y_pred - y)
             self.weights -= self.learning_rate * gradients
 
     def predict(self, X):
         """
-        Предсказывает значения для входных данных X.
+        Предсказывает метки классов входных данных X.
 
         Параметры:
         X : array-like, shape (n_samples, n_features)
@@ -44,10 +50,13 @@ class SGDRegressor(Estimator):
 
         Возвращает:
         array-like, shape (n_samples,)
-            Предсказанные значения.
+            Предсказанные метки классов.
         """
         if self.fit_intercept:
             X_b = np.hstack([np.ones((X.shape[0], 1)), X])
         else:
             X_b = X
-        return X_b.dot(self.weights)
+        
+        y_pred = self._sigmoid(X_b.dot(self.weights))
+        
+        return y_pred
