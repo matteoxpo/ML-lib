e9b8b7567be54f65f00783adb630c74532642582
xpomin <s-hromin@mail.ru>
2024-10-17 14:57:16 +0700

    add sgd classifier and regressor

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,18 @@ from .knn import KNeighborsClassifier, KNeighborsRegressor
 from .grid_searcher import GridSearchCV
 from .estimator import Estimator
 from .linear_regressor import LinearRegressor
+from .linear_classifier import LinearClassifier
+from .sgd_classifier import SGDClassifier
+from .sgd_regressor import SGDRegressor
+from .decision_tree_node import Node
+from .decision_tree_classifier import DecisionTreeClassifier
+from .decision_tree_regressor import DecisionTreeRegressor
 
-__all__ = ['KNeighborsClassifier', 'KNeighborsRegressor', 'GridSearchCV', 'Estimator', 'LinearRegressor']
+
+__all__ = [
+    'Estimator',
+    'KNeighborsClassifier', 'KNeighborsRegressor', 
+    'LinearRegressor', 'LinearClassifier'
+    'SGDClassifier', 'SGDRegressor', 
+    'Node', 'DecisionTreeClassifier', 'DecisionTreeRegressor',
+    'GridSearchCV']
