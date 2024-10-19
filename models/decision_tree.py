fix decision tree, fix gradient boosting, add tests

models/decision_tree.py
@@ -29,35 +29,21 @@ class Node:
     def is_leaf(self):
         return self.left is None and self.right is None
     
-    def print_tree(self, depth=0):
+    def print_tree(self, depth=0, prefix="Root"):
+        # Если это лист, то выводим предсказанное значение
         if self.is_leaf():
-            # Выводим только предсказанное значение для листа
-            indent = " " * (depth * 2)
-            print(f"{indent}{self.predicted_value:.2f}")
+            indent = " " * (depth * 4)
+            print(f"{indent} {prefix} -> [Leaf: {self.predicted_value:.2f}]")
         else:
-            # Выводим информацию о текущем узле
-            indent = " " * (depth * 2)  # Отступы в зависимости от глубины
-            condition = f"[X{self.feature_index}]"
-            print(f"{indent}{condition}")
-            print(f"{indent} {' ' * len(condition)} |")
-            print(f"{indent} ______")
-            print(f"{indent} |      |")
-            print(f"{indent} [<={self.threshold:.2f}]  ", end="")
-            if self.left.is_leaf():
-                print(f"{self.left.predicted_value:.2f}")
-            else:
-                print()  # Переход на новую строку для дальнейшего отображения
-            print(f"{indent} [>{self.threshold:.2f}]  ", end="")
-            if self.right.is_leaf():
-                print(f"{self.right.predicted_value:.2f}")
-            else:
-                print()  # Переход на новую строку для дальнейшего отображения
+            # Иначе выводим условие текущего узла
+            indent = " " * (depth * 4)
+            print(f"{indent} {prefix} -> [X{self.feature_index} <= {self.threshold:.2f}]")
             
-            # Если узлы не являются листьями, выводим их
-            if not self.left.is_leaf():
-                self.left.print_tree(depth + 1)
-            if not self.right.is_leaf():
-                self.right.print_tree(depth + 1)
+            # Рекурсивно выводим левую ветку
+            self.left.print_tree(depth + 1, prefix="L")
+            
+            # Рекурсивно выводим правую ветку
+            self.right.print_tree(depth + 1, prefix="R")
     
     def max_depth(self):
         """
@@ -105,32 +91,8 @@ class DecisionTreeBase(Estimator):
         """Обучает дерево решений на данных X и целевой переменной y."""
         self.root = self._build_tree(X, y)
 
-    def predict(self, X):
-        """Предсказывает значения для набора данных."""
-        return np.array([self._predict_sample(sample) for sample in X])
-
-    def _predict_sample(self, sample):
-        """Предсказывает значение для одного образца."""
-        node = self.root
-        while not node.is_leaf():
-            if sample[node.feature_index] < node.threshold:
-                node = node.left
-            else:
-                node = node.right
-        return node.predicted_value
-
-    @abstractmethod
-    def _calculate_impurity(self, y):
-        """Вычисляет критерий разбиения для текущего узла."""
-
-    @abstractmethod
-    def _best_split(self, X, y):
-        """Находит лучшее разбиение для текущего узла."""
-
     def _build_tree(self, X, y, depth=0):
         """Рекурсивно строит дерево решений."""
-        if (len(y) == 0):
-            raise Exception("asdasd")
         if len(y) < self.min_samples_split or (self.max_depth is not None and depth >= self.max_depth):
             return Node(predicted_value=self._get_leaf_value(y))
 
@@ -150,9 +112,67 @@ class DecisionTreeBase(Estimator):
 
         return Node(feature_index=best_feature, threshold=best_threshold, left=left_child, right=right_child)
 
+    def _best_split(self, X, y):
+        """Находит лучшее разбиение для текущего узла."""
+        best_gain, best_feature_index, best_threshold = None, None, None
+        current_impurity = self._calculate_impurity(y)
+
+        for feature_index in range(X.shape[1]):
+            thresholds = np.unique(X[:, feature_index])
+            for threshold in thresholds:
+                left_indices = X[:, feature_index] < threshold
+                right_indices = X[:, feature_index] >= threshold
+                
+                left_len = len(X[left_indices])
+                right_len = len(X[right_indices]) 
+
+                if left_len < self.min_samples_split or right_len < self.min_samples_split:
+                   continue
+
+                gain = current_impurity - self._calc_weighted_impurity(X, y, left_indices, right_indices)
+                if best_gain is None or gain > best_gain:
+                    best_gain = gain
+                    best_feature_index = feature_index
+                    best_threshold = threshold
+
+        if best_threshold is None: 
+            return None, None, None
+
+        left_len = len(X[:, best_feature_index] < best_threshold)
+        right_len = len(X[:, best_feature_index] >= best_threshold)
+
+        if left_len < self.min_samples_split or right_len < self.min_samples_split:
+            return None, None, None
+
+        return best_feature_index, best_threshold, best_gain
+
     @abstractmethod
+    def _calculate_impurity(self, X, y):
+        """Вычисляет критерий разбиения для текущего узла."""
+    def _calc_weighted_impurity(self,X, y, left_indices, right_indices):
+        return (len(X[left_indices]) * self._calculate_impurity(y[left_indices]) + \
+                len(X[right_indices]) * self._calculate_impurity(y[right_indices])) \
+                    / len(y)
+    
     def _get_leaf_value(self, y):
         """Возвращает значение листа."""
+        return np.mean(y)
+        
+    def predict(self, X):
+        """Предсказывает значения для набора данных."""
+        if X.ndim == 1:
+            return self._predict_sample(X)
+        return np.array([self._predict_sample(sample) for sample in X])
+
+    def _predict_sample(self, sample):
+        """Предсказывает значение для одного образца."""
+        node = self.root
+        while not node.is_leaf():
+            if sample[node.feature_index] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.predicted_value
         
         
 class DecisionTreeRegressor(DecisionTreeBase):
@@ -168,87 +188,25 @@ class DecisionTreeRegressor(DecisionTreeBase):
         else:
             raise Exception("Критерий должен быть 'mse' или 'mae'")
 
-    def _best_split(self, X, y):
-        """Находит лучшее разбиение для текущего узла."""
-        best_impurity = float('inf')
-        best_feature_index, best_threshold, best_gain = None, None, None
-        current_impurity = self._calculate_impurity(y)
-
-        for feature_index in range(X.shape[1]):
-            thresholds = np.unique(X[:, feature_index])
-            for threshold in thresholds:
-                left_indices = X[:, feature_index] < threshold
-                right_indices = X[:, feature_index] >= threshold
-
-                if np.sum(left_indices) < self.min_samples_split or np.sum(right_indices) < self.min_samples_split:
-                    continue
-
-                left_impurity = self._calculate_impurity(y[left_indices])
-                right_impurity = self._calculate_impurity(y[right_indices])
-                weighted_impurity = (len(y[left_indices]) * left_impurity + len(y[right_indices]) * right_impurity) / len(y)
-
-                gain = current_impurity - weighted_impurity
-
-                if best_gain is None or gain > best_gain:
-                    best_gain = gain
-                    best_feature_index = feature_index
-                    best_threshold = threshold
-
-        return best_feature_index, best_threshold, best_gain
-
-    def _get_leaf_value(self, y):
-        """Возвращает среднее значение для листа."""
-        return np.mean(y)
 
 
 class DecisionTreeClassifier(DecisionTreeBase):
-    def __init__(self, criterion='gini', max_depth=None, min_samples_split=2):
+    def __init__(self, criterion='gini', max_depth=None, min_samples_split=2, probabilities=False):
         super().__init__(criterion, max_depth, min_samples_split)
+        self.probabilities = probabilities
+
+    def fit(self, X, y):
+        """Обучает дерево решений на данных X и целевой переменной y."""
+        y = np.clip(y, 0, 1)
+        self.root = self._build_tree(X, y)
 
     def _calculate_impurity(self, y):
         """Вычисляет критерий разбиения для текущего узла."""
         class_labels, counts = np.unique(y, return_counts=True)
         probabilities = counts / counts.sum()
-
         if self.criterion == 'gini':
             return 1 - np.sum(probabilities ** 2)
         elif self.criterion == 'entropy':
             return -np.sum(probabilities * np.log2(probabilities + 1e-9))
         else:
             raise Exception('Критерий должен быть gini или entropy')
-
-    def _best_split(self, X, y):
-        """Находит лучшее разбиение для текущего узла."""
-        best_impurity = float('inf')
-        best_gain = 0
-        best_feature_index = None
-        best_threshold = None
-
-        current_impurity = self._calculate_impurity(y)
-
-        for feature_index in range(X.shape[1]):
-            thresholds = np.unique(X[:, feature_index])
-            for threshold in thresholds:
-                left_indices = X[:, feature_index] <= threshold
-                right_indices = X[:, feature_index] > threshold
-
-                if np.sum(left_indices) < self.min_samples_split or np.sum(right_indices) < self.min_samples_split:
-                    continue
-
-                left_impurity = self._calculate_impurity(y[left_indices])
-                right_impurity = self._calculate_impurity(y[right_indices])
-                weighted_impurity = (np.sum(left_indices) / len(y)) * left_impurity + (np.sum(right_indices) / len(y)) * right_impurity
-
-                gain = current_impurity - weighted_impurity
-
-                if gain > best_gain:
-                    best_gain = gain
-                    best_feature_index = feature_index
-                    best_threshold = threshold
-
-        return best_feature_index, best_threshold, best_gain
-
-    def _get_leaf_value(self, y):
-        """Возвращает наиболее часто встречающуюся метку для листа."""
-        return np.bincount(y).argmax()
-

models/ensemble/gradient_boosting.py
@@ -1,43 +1,87 @@
 import numpy as np
-from estimator import Estimator
-from models import DecisionTreeRegressor, DecisionTreeClassifier
+from models import DecisionTreeRegressor, DecisionTreeClassifier, Estimator
 from abc import abstractmethod
+from utils import sigmoid
 
 class GradientBoostingBase(Estimator):
-    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth=3):
+    def __init__(self, n_estimators=100, learning_rate=0.1, estimator_class=None, **estimator_params):
+        """
+        n_estimators: количество моделей для бустинга
+        learning_rate: скорость обучения
+        estimator_class: класс модели, который будет использован (например, DecisionTreeClassifier)
+        estimator_params: параметры, которые будут переданы в каждую модель (например, max_depth для дерева)
+        """
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
-        self.max_depth = max_depth
-        self.trees = []
+        self.estimator_class = estimator_class
+        self.estimator_params = estimator_params
+        self.estimators = []
         
     def fit(self, X, y):
         y_pred = np.full(y.shape, np.mean(y))
 
         for _ in range(self.n_estimators):
-            diff = y - y_pred
 
-            tree = self._get_decision_tree()
-            tree.fit(X, diff)
+            estimator = self.estimator_class(**self.estimator_params)
+            estimator.fit(X, y - y_pred)
 
-            y_pred += self.learning_rate * tree.predict(X)
+            y_pred += self.learning_rate * estimator.predict(X)
 
-            self.trees.append(tree)
+            self.estimators.append(estimator)
             
     def predict(self, X):
-        y_pred = np.zeros(X.shape[0])
-        for tree in self.trees:
-            y_pred += self.learning_rate * tree.predict(X)
-        return y_pred
-        
-    @abstractmethod
-    def _get_decision_tree(self):
+        """Предсказывает значения для набора данных."""
+        if X.ndim == 1:
+            return self._predict_sample(X)
+        return np.array([self._predict_sample(sample) for sample in X])
+    
+    @abstractmethod        
+    def _predict_sample(self, X):
         pass
 
-class GradientBoostingClassifier(GradientBoostingBase):
+class GradientBoostingBaseClassificator(GradientBoostingBase):
+    def __init__(self, n_estimators=100, learning_rate=0.1, g_threshold = 0.5, g_probabilities = False, estimator_class=None, **estimator_params):
+        super().__init__(n_estimators=n_estimators, learning_rate=learning_rate, estimator_class=estimator_class, **estimator_params)
+        self.g_probabilities = g_probabilities
+        self.g_threshold = g_threshold
     
-    def _get_decision_tree(self):
-        return DecisionTreeClassifier(max_depth= self.max_depth)
+    def _predict_sample(self, X):
+        y_pred = 0
 
-class GradientBoostingRegressor(GradientBoostingBase):
-    def _get_decision_tree(self):
-        return DecisionTreeRegressor(max_depth= self.max_depth)
+        for tree in self.estimators:
+            y_pred += self.learning_rate * tree.predict(X)
+        if self.g_probabilities:
+            return y_pred
+        return y_pred > self.g_threshold
+    
+class GradientBoostingBaseRegressor(GradientBoostingBase):
+    def _predict_sample(self, X):
+        y_pred = np.zeros(X.shape[0])
+        for tree in self.estimators:
+            y_pred += self.learning_rate * tree.predict(X)
+        return y_pred
+
+
+class GradientBoostingDecisionTreeClassifier(GradientBoostingBaseClassificator):
+    
+    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth = 3,g_threshold = 0.5, g_probabilities = False, min_samples_split = 2 ):
+        """
+        n_estimators: количество моделей для бустинга
+        learning_rate: скорость обучения
+        estimator_class: класс модели, который будет использован (например, DecisionTreeClassifier)
+        estimator_params: параметры, которые будут переданы в каждую модель (например, max_depth для дерева)
+        """
+        super().__init__(n_estimators=n_estimators, learning_rate=learning_rate, 
+                         g_probabilities = g_probabilities, g_threshold = g_threshold, 
+                         estimator_class=DecisionTreeClassifier, 
+                            max_depth = max_depth, probabilities = True, min_samples_split = min_samples_split)
+
+class GradientBoostingDecisionTreeRegressor(GradientBoostingBaseRegressor):
+    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth = 3):
+        """
+        n_estimators: количество моделей для бустинга
+        learning_rate: скорость обучения
+        estimator_class: класс модели, который будет использован (например, DecisionTreeClassifier)
+        estimator_params: параметры, которые будут переданы в каждую модель (например, max_depth для дерева)
+        """
+        super().__init__(n_estimators=n_estimators, learning_rate=learning_rate, estimator_class=DecisionTreeRegressor, max_depth = max_depth)